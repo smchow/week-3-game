1155aca035a57a7e78b8cfebb995362c069a5c5f
smchow <sunita.m.chowdhury@gmail.com>
2016-11-13 17:23:24 -0500

    Moved functions to external js file, added win/loss logic

--- javascript/game.js
+++ javascript/game.js
@@ -1,10 +1,49 @@
-var wins = 0;
-var losses = 0;
-var ties = 0;
-var guesses = [];
+	
 
-document.onkeyup = function(event){
-	var key = event.key;
-	document.write("Welcome to Guess the Scientist!<br/>" + key );
-	console.log(key );
-}
+      /*   */
+
+      function displayResults(word, disp, k, guesses){
+        var string = '<p>' + disp + '</p>';
+        string += '<p>User chose: ' + k + '<p>';
+        string += '<p>Wins: ' + wins; + '</p>';
+        string += '<p>Your Guesses: ' + guesses ; + '</p>';
+        string += '<p>Number of Guesses used: ' + guesses.length + ' of 10</p>';
+        won = hasWon(disp);
+   		console.log(" hasWon " + won);
+        if (won){
+          		string += '<p>You won</p>';
+          	} else {
+          		string += '<p>Guess the next letter</p>';
+          	}
+        display.innerHTML = string;
+      }
+
+      /* 
+      */
+
+      function getPartWord(wordarr){
+      			dispWord ="";
+      			for(i=0; i< wordarr.length; i++){
+      					
+      					if (guesses.indexOf(wordarr[i]) > -1){
+
+      						dispWord = dispWord + wordarr[i] + " ";
+      					}else{
+      						dispWord = dispWord + "- ";
+      					}
+      					
+      			}
+      			console.log(dispWord);
+      			return dispWord;
+      }
+
+      function hasWon(dispWord){
+      	console.log(dispWord.indexOf("-"));
+      	if (dispWord.indexOf("-") > -1){
+      		return false;
+      	}else{
+      		return true;
+      	}
+      }
+
+      
