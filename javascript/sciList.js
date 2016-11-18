2f0ec034bebb2cbf3dd4be95edfa23595b72e4de
smchow <sunita.m.chowdhury@gmail.com>
2016-11-18 13:46:38 -0500

    Added extra css styling

--- javascript/sciList.js
+++ javascript/sciList.js
@@ -1,86 +1,32 @@
-var text =  '{ "scientists" :[{"name":"einstein","fname":"Einstein, Albert", "ach": "Theories of relativity and E = mc2", "url":"www.famousscientists.com/einstein"},'+
-'{"name":"bell","fname":"Bell, Alexander Graham", "ach": "Inventor of the telephone", "url":"www.famousscientists.com/bell"},'+
-'{"name":"lovelace","fname":"Lovelace, Ada", "ach": "Mother of computing science", "url":"www.famousscientists.com/lovelace"},'+
-'{"name":"bohr", "fname":"Bohr, Niels", "ach": "Quantum mechanics & the atom"},'+
-'{"name":"pythagoras", "fname":"Pythagoras", "ach": "The first rigorous mathematics"},'+
-'{"name":"newton", "fname":"Newton, Isaac", "ach": "Gravity, light, the laws of motion and calculus"},'+	
-'{"name":"Nobel", "fname":"Nobel, Alfred", "ach": "Invented dynamite and those prizes"},'+	 
-'{"name":"Curie","fname":"Curie, Marie", "ach": "Discoverer of elements. Radioactivity pioneer", "url":"www.famousscientists.com/curie"}]}';
 
-	//	'{"name":"", "fname":"", "ach": ""},'+		 
-var obj = JSON.parse(text);
-
-/*function addDetails(i){
-	console.log("i = "+ i);
-	document.getElementById("details").innerHTML =
-	obj.scientists[i].name + "<br>" +
-	obj.scientists[i].ach + "<br>" +
-	obj.scientists[i].url;
-}*/
-/* 
-**Generate a random number and 
-**Return the scientist at that position from the JSON
-*/
-function getRandomName(){
-	sci_ind = Math.floor(Math.random()*obj.scientists.length);
-	sci = obj.scientists[sci_ind];
-	var pickedSci = new scientist(sci.name, sci.ach, sci.fname, sci.url);
-	return pickedSci;
-}
-
+/* Creating scientist object and all related methods*/
 function scientist(name, ach, fname, url) {
   this.name = name.toLowerCase();
   this.ach = ach;
   this.fname = fname;
+  this.url = url;
   this.guesses = [];
   this.guessCorrect = false;
   this.numGuesses = 10;
   this.imgFile = "./images/"+this.name + ".jpg";
   this.error = " ";
 
-  this.displayDetails = function () {
-  	console.log(this.imgFile);
-    details.innerHTML =  "Name: " +
-           this.fname +
-           "<br/><img class='detailImg' src='" + this.imgFile + "'</img>" +
-           "<br/> Famous for: " +
-           this.ach ;
-  };
 
-  this.getPartWord = function(){
-      			dispWord ="";
-      			wordarr = this.name;
-      			console.log(this.guesses);
-
-      			for(i=0; i< wordarr.length; i++){
-      					
-      					if (this.guesses.indexOf(wordarr[i]) > -1){
-
-      						dispWord = dispWord + wordarr[i] ;
-      					}else{
-      						dispWord = dispWord + "- ";
-      					}
-      					
-      			}
-            	console.log(wordarr + "**" + dispWord);
-            	if (wordarr==dispWord){
-            		this.whenCorrect();
-      			}
-      			return dispWord;
-      };
-
+  /* The user has entered a letter . So start playing and give feedback to the user*/
     this.playGame =  function(key){
-      console.log("Should I continue" + this.guessCorrect + "Guesses = "  + this.numGuesses);
+      //console.log("Should I continue" + this.guessCorrect + "Guesses = "  + this.numGuesses);
       if (this.numGuesses > 0 && !(this.guessCorrect) ){
             this.newGuess(key);
           	display.innerHTML = this.getPartWord() + "<br/> <br/>Your Letter Guesses: " + this.guesses + 
                                 "<br/><br/> You have " +  + this.numGuesses + " guesses remaining!";
             gameStatus.innerHTML = "<br/> # of Correct Guesses : " + wins + "<br/> # of Incorrect Guesses : " + 
                             losses + "<br/>" + this.error;
-
      		}
  		};
 
+    /*Update the guesses array and total number of guesses variable.
+      Check if the user has exceeded the number of guesses and determine
+      if they have lost. Win logic in getPartWord() */
    this.newGuess = function(key){
    	  if (key==""){
    	  		return;
@@ -103,6 +49,27 @@ function scientist(name, ach, fname, url) {
        }
    }
 
+   /* Check which letters have been guessed correctly and display the partially completed
+     word on the screen. Check if the player has won in the end*/
+  this.getPartWord = function(){
+            dispWord ="";
+            wordarr = this.name;
+
+            for(i = 0; i < wordarr.length; i++){          
+                if (this.guesses.indexOf(wordarr[i]) > -1){
+
+                  dispWord = dispWord + wordarr[i] ;
+                }else{
+                  dispWord = dispWord + "- ";
+                }       
+            }
+              if (wordarr==dispWord){
+                this.whenCorrect();
+            }
+            return dispWord;
+      };
+
+
    /* When the user guesses the correct word increment number of wins
    */
    this.whenCorrect = function(){
@@ -119,5 +86,42 @@ function scientist(name, ach, fname, url) {
          } else {   
          	return false;  
          }  
-      }  
+      }
+
+
+  /* Display some extra details about the scientist.
+  Called once the word has been guessed or user has used up all guesses.*/
+  this.displayDetails = function () {
+    details.innerHTML =  "Name: " +
+           this.fname +
+           "<br/><img class='detailImg' src='" + this.imgFile + "'</img>" +
+           "<br/>Famous for: " + this.ach ;
+           // +"<br/>For more information " +
+           //'<a href="'+this.url+'" target="_blank">click here.</a>'
+  };
+  
+}// End tag - scientist object
+
+
+var text =  '{ "scientists" :[{"name":"einstein","fname":"Einstein, Albert", "ach": "Theories of relativity and E = mc2", "url":"www.famousscientists.com/einstein/"},'+
+'{"name":"bell","fname":"Bell, Alexander Graham", "ach": "Invention of the telephone", "url":"www.famousscientists.com/bell/"},'+
+'{"name":"lovelace","fname":"Lovelace, Ada", "ach": "Pioneer/Mother of computing science", "url":"http://www.famousscientists.org/ada-lovelace/"},'+
+'{"name":"bohr", "fname":"Bohr, Niels", "ach": "Quantum mechanics & the atom", "url":"http://www.famousscientists.org/niels-bohr/"},'+
+'{"name":"pythagoras", "fname":"Pythagoras", "ach": "Pythagorean theorem.  Philosopher and mathematician", "url":"www.famousscientists.com/pythagoras/"},'+
+'{"name":"newton", "fname":"Newton, Isaac", "ach": "The laws of motion and gravity. Theory of color. Calculus.", "url":"http://www.famousscientists.org/isaac-newton/"},'+  
+'{"name":"Nobel", "fname":"Nobel, Alfred", "ach": "Invention of dynamite and those prizes", "url":"http://www.famousscientists.org/alfred-nobel/"},'+  
+'{"name":"Curie","fname":"Curie, Marie", "ach": "Discovery of elements. Radioactivity pioneer", "url":"www.famousscientists.com/curie/"}]}';
+//  '{"name":"", "fname":"", "ach": ""},'+    
+
+
+var obj = JSON.parse(text);
+/* 
+**Generate a random number and 
+**Return the scientist at that position from the JSON
+*/
+function getRandomName(){
+  sci_ind = Math.floor(Math.random()*obj.scientists.length);
+  sci = obj.scientists[sci_ind];
+  var pickedSci = new scientist(sci.name, sci.ach, sci.fname, sci.url);
+  return pickedSci;
 }
